2db0eead328a210f935586dc9637c9fe2c320fe7
Sara-Jayne Farmer <sara.farmer@btinternet.com>
2013-04-19 09:43:35 +0100

    first commit

---
+++ uncountry/README.md
@@ -0,0 +1,5 @@
+uncountry
+=======
+
+Global country name standards (e.g. unstats, FIPS, stanag, cia world factbook). 
+Not ready for use yet.
---
+++ uncountry/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='uncountry',
+      version='0.1',
+      description='Global country name standards',
+      author='Sara-Jayne Farmer',
+      author_email='sara@changeassembly.com',
+      url='',
+      packages=['uncountry'],
+      )
